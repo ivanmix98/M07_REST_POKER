ab2a2763f684d160feb7da6dbcbc26ee0374eb1d
ivfosa1819daw2 <ivfosa1819daw2@gmail.com>
2019-02-19 17:23:19 +0100

    Creación de array J1

--- controladors.js
+++ controladors.js
@@ -1,52 +1,55 @@
 var b = require('./baralla');
 var j = require('./classes/Jugador');
 var p = require('./classes/Partida');
-var Partidas=[];
+var Partidas = [];
+var CartasJ1 = [];
 
-function IniciarJoc(req,res){
+function IniciarJoc(req, res) {
     let codiP = req.params.codiPartida;
 
     let Partida = Partidas.find(part => part.id === codiP);
 
-    if(Partida != null){ res.status(400).send("Ja existeix Aquesta Partida"); }
+    if (Partida != null) { res.status(400).send("Ja existeix Aquesta Partida"); }
 
-    else{
+    else {
         let newBaralla = shuffle(b.baralla);
-        let newPartida = new p.Partida(codiP,newBaralla,1,0);
+        let newPartida = new p.Partida(codiP, newBaralla, 1, 0);
         Partidas.push(newPartida);
         res.status(200).send(newPartida);
     }
 }
-function ObtenirCarta(req,res){
+function ObtenirCarta(req, res) {
     let codiP = req.params.codiPartida;
 
     let currentPartida = Partidas.find(part => part.id === codiP);
 
-    if(currentPartida == null){ res.status(404).send("No s'ha trobat la partida"); }
-    else{
+    if (currentPartida == null) { res.status(404).send("No s'ha trobat la partida"); }
+    else {
         let cartes = currentPartida.cartes;
-        res.send(cartes.pop());
+        let unacarta = cartes.pop();
+        CartasJ1.push(unacarta);
+        res.status(200).send(unacarta);
     }
 }
-function MostrarCartes(req,res){
+function MostrarCartes(req, res) {
     let codiP = req.params.codiPartida;
     let currentPartida = Partidas.find(part => part.id === codiP);
 
-    if(currentPartida == null){ res.status(404).send("No s'ha trobat la partida"); }
-    else{
+    if (currentPartida == null) { res.status(404).send("No s'ha trobat la partida"); }
+    else {
         res.send(currentPartida.cartesTaula);
     }
 }
-function TirarCarta(req,res){
+function TirarCarta(req, res) {
     res.send('TirarCarta');
 }
-function MoureJugadorAposta(req,res){
+function MoureJugadorAposta(req, res) {
     res.send('Moure Jugador Aposta');
 }
-function MoureJugadorPassa(req,res){
+function MoureJugadorPassa(req, res) {
     res.send('Moure Jugador Passa');
 }
-function AcabarJoc(req,res){
+function AcabarJoc(req, res) {
     res.send('Acabar Joc');
 }
 
@@ -64,10 +67,10 @@ function shuffle(a) {
     return a;
 }
 
-exports.IniciarJoc          = IniciarJoc;
-exports.ObtenirCarta        = ObtenirCarta;
-exports.MostrarCartes       = MostrarCartes;
-exports.TirarCarta          = TirarCarta;
-exports.MoureJugadorAposta  = MoureJugadorAposta;
-exports.MoureJugadorPassa   = MoureJugadorPassa;
-exports.AcabarJoc           = AcabarJoc;
+exports.IniciarJoc = IniciarJoc;
+exports.ObtenirCarta = ObtenirCarta;
+exports.MostrarCartes = MostrarCartes;
+exports.TirarCarta = TirarCarta;
+exports.MoureJugadorAposta = MoureJugadorAposta;
+exports.MoureJugadorPassa = MoureJugadorPassa;
+exports.AcabarJoc = AcabarJoc;
