2e4050238f377ac24ca82d1918a8f9d987bc08b0
ivfosa1819daw2 <ivfosa1819daw2@gmail.com>
2019-02-25 16:01:21 +0100

    JWT implementado

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const bodyParser = require('body-parser');
 const app=express();
 const c = require('./controladors');
 
+
+const jwt = require("jsonwebtoken");
+const usuaris = [
+    {id:1, nom:"jugador1", password:"1234"},
+    {id:2, nom:"jugador2", password:"5678"}
+]
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -19,3 +28,19 @@ app.put('/moureJugador/:codiPartida/passa', c.MoureJugadorPassa);
 app.delete('/acabarJoc/:codiPartida', c.AcabarJoc);
 
 app.listen(3000, ()=>console.log('inici servidor http://localhost:3000'));
+
+app.post("/login", (req, res) => {
+    if (!req.body.nom || !req.body.password) {
+        res.status(400).send("Error");
+        return;
+    }
+    const usuari = usuaris.find((u) => {
+        return u.nom === req.body.nom && u.password === req.body.password;
+    });
+
+    const token = jwt.sign({
+        sub: usuari.id,
+        usuari: usuari.nom
+    }, "clau", { expiresIn: "3 hours" });
+    res.status(200).send({ success: 'Welcome to the JWT Auth', access_token: token })
+});
